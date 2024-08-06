878d273759ffd846c9147a84d577f120caff469e
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-06 18:46:25 -0300

    🚚 Refactor: add container into ui folder and use in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { Container } from '~/components/ui/Container';
+
 export default function HomePage() {
 	return (
 		<div>
 			<main>
-				<h1 className="text-white">Sanity Blog</h1>
-				<h1 className="text-base-100">Sanity Blog</h1>
-				<h1 className="text-base-200">Sanity Blog</h1>
-				<h1 className="text-base-300">Sanity Blog</h1>
+				<Container>
+					<h1 className="text-white">Sanity Blog</h1>
+					<h1 className="text-base-100">Sanity Blog</h1>
+					<h1 className="text-base-200">Sanity Blog</h1>
+					<h1 className="text-base-300">Sanity Blog</h1>
+				</Container>
 			</main>
 		</div>
 	);
