7446846ff412643e1592993b5b6a9163fdfc9753
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-06 18:43:32 -0300

    ✨ Feat: create container component to use in page

---
+++ src/components/Container.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from 'react';
+import { cn } from '~/lib/utils';
+
+interface ContainerProps {
+	children: ReactNode;
+	className?: string;
+}
+
+export function Container({ children, className }: ContainerProps) {
+	return (
+		<div
+			className={cn(
+				'm-auto max-w-screen-2xl 3xl:px-0 px-[1.875rem] lg:px-[11.25rem]',
+				className
+			)}
+		>
+			{children}
+		</div>
+	);
+}
