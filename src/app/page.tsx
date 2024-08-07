006b2e792c8fbfef7d94629c01dc4fae03bbc2cb
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-06 23:02:10 -0300

    🔥 Cleanup: remove desnecessary code lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ export default function HomePage() {
 		<div>
 			<main>
 				<h1 className="text-white">Sanity Blog</h1>
-				<h1 className="text-base-100">Sanity Blog</h1>
-				<h1 className="text-base-200">Sanity Blog</h1>
-				<h1 className="text-base-300">Sanity Blog</h1>
 			</main>
 		</div>
 	);
