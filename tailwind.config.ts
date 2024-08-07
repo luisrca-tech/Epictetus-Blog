87c779394ae52aadd64637e95e661a87c52a6666
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-06 22:59:05 -0300

    ➕ Build: install shadcn sheet to do menu navigation header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
 	theme: {
 		colors: {
 			white: 'rgb(var(--white) / <alpha-value>)',
+			black: 'rgb(var(--black) / <alpha-value>)',
 			base: {
 				100: 'rgb(var(--100) / <alpha-value>)',
 				200: 'rgb(var(--200) / <alpha-value>)',
@@ -18,8 +19,26 @@ export default {
 		extend: {
 			fontFamily: {
 				inter: ['var(--font-inter)']
+			},
+			screens: {
+				'2xl': '87.5rem',
+				'3xl': '100rem'
 			}
+		},
+		keyframes: {
+			'accordion-down': {
+				from: { height: '0' },
+				to: { height: 'var(--radix-accordion-content-height)' }
+			},
+			'accordion-up': {
+				from: { height: 'var(--radix-accordion-content-height)' },
+				to: { height: '0' }
+			}
+		},
+		animation: {
+			'accordion-down': 'accordion-down 0.2s ease-out',
+			'accordion-up': 'accordion-up 0.2s ease-out'
 		}
 	},
-	plugins: []
+	plugins: [require('tailwindcss-animate')]
 } satisfies Config;
