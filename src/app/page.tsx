6e401eb97770242ace26ec7748fbf28419c8961d
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-22 22:28:23 -0300

    🚚 Refactor: rename page of posts and resolve imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { PostsBlog } from '~/components/block/PostsBlog';
+import { BlogList } from '~/components/block/BlogList';
 import { Container } from '~/components/ui/Container';
 
 export default function HomePage() {
@@ -6,7 +6,7 @@ export default function HomePage() {
 		<div>
 			<main>
 				<Container className="pt-7 pb-8">
-					<PostsBlog />
+					<BlogList />
 				</Container>
 			</main>
 		</div>
