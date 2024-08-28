8d3aa665405ea47d91a2917f26e088aece711bbe
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-28 12:48:37 -0300

    🐛 Fix: ignore biome lint warning from type any

--- src/components/block/RichTextComponents.tsx
+++ src/components/block/RichTextComponents.tsx
@@ -1,9 +1,5 @@
 import { SanityImage } from '../SanityImage';
 
-type HeadingProps = {
-	children: React.ReactNode;
-};
-
 const RichTextComponents = {
 	types: {
 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -19,42 +15,50 @@ const RichTextComponents = {
 		}
 	},
 	list: {
-		bullet: ({ children }: HeadingProps) => (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		bullet: ({ children }: any) => (
 			<ul className="ml-10 list-disc space-y-5 px-3 py-5 md:px-6">
 				{children}
 			</ul>
 		),
-		number: ({ children }: HeadingProps) => (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		number: ({ children }: any) => (
 			<ol className="mt-lg list-decimal px-3 md:px-4">{children}</ol>
 		)
 	},
 	block: {
-		normal: ({ children }: HeadingProps) => (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		normal: ({ children }: any) => (
 			<p className="flex max-w-[44.25rem] items-center justify-center pb-10 text-white text-xl leading-8">
 				{children}
 			</p>
 		),
-		h1: ({ children }: HeadingProps) => (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		h1: ({ children }: any) => (
 			<h1 className="flex max-w-[44.25rem] items-center justify-center py-10 text-center text-3xl text-white/60 leading-8">
 				{children}
 			</h1>
 		),
-		h2: ({ children }: HeadingProps) => (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		h2: ({ children }: any) => (
 			<h2 className="flex max-w-[44.25rem] items-center justify-center pb-10 text-white text-xl leading-8">
 				{children}
 			</h2>
 		),
-		h3: ({ children }: HeadingProps) => (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		h3: ({ children }: any) => (
 			<h3 className="flex max-w-[44.25rem] items-center justify-center pb-10 text-white text-xl leading-8">
 				{children}
 			</h3>
 		),
-		h4: ({ children }: HeadingProps) => (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		h4: ({ children }: any) => (
 			<h4 className="flex max-w-[44.25rem] items-center justify-center pb-10 text-white text-xl leading-8">
 				{children}
 			</h4>
 		),
-		blockquote: ({ children }: HeadingProps) => (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		blockquote: ({ children }: any) => (
 			<blockquote className="mb-10 max-w-[44.25rem] border-l-4 border-l-[#F7AB0A] py-5 pl-5 text-white">
 				{children}
 			</blockquote>
