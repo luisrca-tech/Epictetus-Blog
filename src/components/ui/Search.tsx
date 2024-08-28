8f382fcc6282b19c01233966d08440ad5d524d1d
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-28 18:03:36 -0300

    🚧 Working...

---
+++ src/components/ui/Search.tsx
@@ -0,0 +1,11 @@
+export function SearchInput() {
+	return (
+		<>
+			<input
+				type="text"
+				placeholder="Search"
+				className="rounded-[6.25rem] bg-base-200 p-[0.5625rem] pl-9 text-base text-white"
+			/>
+		</>
+	);
+}
--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -20,3 +20,17 @@ export const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]{
   slug,
   image,
 }`;
+
+export const POSTS_BY_CATEGORY_QUERY = groq`*[_type == "post" && defined(slug.current) && $category in categories[]->title] | order(publishedAt desc) {
+    featured,
+    _id,
+    description,
+    title,
+    publishedAt,
+    "slug": slug.current,
+    mainImage { ..., asset -> {..., metadata}},
+    author -> {image { ..., asset -> {..., metadata}}, name, role},
+    categories[]->{
+      title
+    },
+}`;
