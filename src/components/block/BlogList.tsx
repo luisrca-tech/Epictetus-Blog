a456ff8ca200acb5811dba7e4c33dc7395369518
Luis Felipe <luisrochacruzalves@gmail.com>
2024-09-10 06:58:08 -0300

    ♻️ Refactor: get posts of home page and validate that to do the filter

--- src/components/block/BlogList.tsx
+++ src/components/block/BlogList.tsx
@@ -3,11 +3,18 @@
 import { useAtom } from 'jotai';
 import { filteredSearchPosts } from '~/atom/filteredSearchPosts';
 import { cn } from '~/lib/utils';
+import type { POSTS_QUERYResult } from '~/types/PostsQueryResult.type';
 import { ArticleCard } from '../ui/ArticleCard';
 
-export function BlogList() {
-	const [posts] = useAtom(filteredSearchPosts);
-	const featuredPost = posts.find((post) => post.featured);
+type Props = {
+	posts: POSTS_QUERYResult;
+};
+
+export function BlogList({ posts }: Props) {
+	const [filteredPosts] = useAtom(filteredSearchPosts);
+	const finalData = filteredPosts.length > 0 ? filteredPosts : posts;
+
+	const featuredPost = finalData.find((post) => post.featured);
 
 	return (
 		<div className="flex flex-col md:grid md:grid-cols-3 md:gap-[1.875rem] lg:grid lg:grid-cols-3 lg:gap-[1.875rem]">
@@ -16,7 +23,7 @@ export function BlogList() {
 					<ArticleCard post={featuredPost} />
 				</div>
 			)}
-			{posts.map(
+			{finalData.map(
 				(post) =>
 					!post.featured && (
 						<div
