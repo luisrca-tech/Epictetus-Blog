28584450891393db2903c04c299a49e236743426
Luis Felipe <luisrochacruzalves@gmail.com>
2024-09-11 22:51:47 -0300

    ✨ Feat: transform searchTerm in global state to use in multiple pages

--- src/components/SearchArticle.tsx
+++ src/components/SearchArticle.tsx
@@ -1,24 +1,27 @@
 'use client';
 
-import { useState } from 'react';
+import { useAtom } from 'jotai';
+import { searchTerm } from '~/atom/searchTerm';
 import { useSearchPosts } from '~/hooks/useSearchPosts';
 
 export function SearchArticle() {
-	const [searchTerm, setSearchTerm] = useState('');
+	const [searchValue, setSearchValue] = useAtom(searchTerm);
 
-	useSearchPosts(searchTerm);
+	useSearchPosts();
 
 	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setSearchTerm(event.target.value);
+		setSearchValue(event.target.value);
 	};
 
 	return (
-		<input
-			type="text"
-			placeholder="Search"
-			className="rounded-[6.25rem] bg-base-200 p-[0.5625rem] pl-9 text-base text-white"
-			value={searchTerm}
-			onChange={handleSearch}
-		/>
+		<div>
+			<input
+				type="text"
+				placeholder="Search"
+				className="rounded-[6.25rem] bg-base-200 p-[0.5625rem] pl-9 text-base text-white"
+				value={searchValue}
+				onChange={handleSearch}
+			/>
+		</div>
 	);
 }
--- src/hooks/useSearchPosts.ts
+++ src/hooks/useSearchPosts.ts
@@ -1,27 +1,31 @@
 import { useAtom } from 'jotai';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { filteredSearchPosts } from '~/atom/filteredSearchPosts';
+import { searchTerm } from '~/atom/searchTerm';
 import { client } from '~/sanity/lib/client';
 import { POSTS_QUERY, SEARCH_POSTS_QUERY } from '~/sanity/lib/queries';
 import type { POSTS_QUERYResult } from '~/types/PostsQueryResult.type';
 import type { SEARCH_POSTS_QUERYResult } from '~/types/SearchPostsQueryResult.type';
 
-export function useSearchPosts(searchTerm: string) {
+export function useSearchPosts() {
 	const [, setFilteredSearchPosts] = useAtom(filteredSearchPosts);
+	const [searchValue] = useAtom(searchTerm);
+	const [hasResults, setHasResults] = useState(true);
 
 	useEffect(() => {
 		const abortController = new AbortController();
 		const signal = abortController.signal;
 		const fetchPosts = async () => {
-			if (searchTerm) {
+			if (searchValue) {
 				const filteredPosts = await client.fetch<SEARCH_POSTS_QUERYResult>(
 					SEARCH_POSTS_QUERY,
 					{
-						title: `*${searchTerm}*`
+						title: `*${searchValue}*`
 					},
 					{ signal, next: { tags: ['blog'] } }
 				);
 				setFilteredSearchPosts(filteredPosts);
+				setHasResults(filteredPosts.length > 0);
 			} else {
 				const allPosts = await client.fetch<POSTS_QUERYResult>(
 					POSTS_QUERY,
@@ -29,6 +33,7 @@ export function useSearchPosts(searchTerm: string) {
 					{ next: { tags: ['blog'] } }
 				);
 				setFilteredSearchPosts(allPosts);
+				setHasResults(allPosts.length > 0);
 			}
 		};
 
@@ -37,5 +42,7 @@ export function useSearchPosts(searchTerm: string) {
 		return () => {
 			abortController.abort();
 		};
-	}, [searchTerm, setFilteredSearchPosts]);
+	}, [searchValue, setFilteredSearchPosts]);
+
+	return hasResults;
 }
